f774bc2e8909778813e89e77972e6d8ef182a216
Ryo Manzoku <manzoku.ryo@gmail.com>
2023-09-15 01:38:50 +0900

    commit

---
+++ go/cmd/container/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	env := os.Environ()
+	for _, e := range env {
+		fmt.Println(e)
+	}
+}
---
+++ go/go.mod
@@ -0,0 +1,3 @@
+module github.com/rmanzoku/ci-testing/go
+
+go 1.21.1
